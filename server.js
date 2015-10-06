7301d2eefcfc68695168df3f3aaba7180927e69b
Pranay Singh <sng.pranay@gmail.com>
2015-10-06 10:28:24 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,60 @@
+/* Implementation of webrtc on node js */
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+http.listen(3000, function() {
+  console.log('listining on port 3000');
+});
+
+app.use('/', express.static(__dirname + '/views'));
+app.use('/', express.static(__dirname + '/assets/javascript'));
+
+app.get('/', function(req,res){
+  res.sendfile('index');
+});
+
+
+
+io.sockets.on('connection', function (socket){
+
+  // convenience function to log server messages on the client
+	function log(){
+		var array = [">>> Message from server: "];
+	  for (var i = 0; i < arguments.length; i++) {
+	  	array.push(arguments[i]);
+	  }
+	    socket.emit('log', array);
+	}
+
+	socket.on('message', function (message) {
+		log('Got message:', message);
+    // for a real app, would be room only (not broadcast)
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.on('create or join', function (room) {
+		var numClients = io.sockets.clients(room).length;
+
+		log('Room ' + room + ' has ' + numClients + ' client(s)');
+		log('Request to create or join room ' + room);
+
+		if (numClients === 0){
+			socket.join(room);
+			socket.emit('created', room);
+		} else if (numClients === 1) {
+			io.sockets.in(room).emit('join', room);
+			socket.join(room);
+			socket.emit('joined', room);
+		} else { // max two clients
+			socket.emit('full', room);
+		}
+		socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
+		socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+
+	});
+
+
+});
